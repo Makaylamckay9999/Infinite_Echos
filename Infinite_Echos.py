82154cded45a31d951656792fee470f36016e499
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-09 14:09:54 -0500

    Update Infinite_Echos.py
    Was able to broadcast repetition 50 times, 100 times and 1000 times

--- Infinite_Echos.py
+++ Infinite_Echos.py
@@ -1,1 +1,8 @@
 print("Cast Thunderwave!" *10)
+#*Was able to have to cast 10 times
+print("Cast Wish!" *50)
+#*was able to cast 10 times
+print ("Cast Crown Of Stars!" *100 )
+#* was able to cast 100 times
+print ("Cast Time Stop!" *1000)
+#* was able to cast 1000 but won't be able to include everything in the screenshot
